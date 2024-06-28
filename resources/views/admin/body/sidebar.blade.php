5da6ef8738bc29f24efadabf5ed41d939edc9bc0
Hoàng Nguyễn Lê <nguyenlehoang20031@gmail.com>
2024-06-28 10:49:52 +0700

    Add CRUD for Admin User

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -85,13 +85,13 @@
             </li>
             <li class="nav-item nav-category">Role & Permission</li>
             <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#advancedUI" role="button" aria-expanded="false"
-                    aria-controls="advancedUI">
+                <a class="nav-link" data-bs-toggle="collapse" href="#rolePermissions" role="button" aria-expanded="false"
+                    aria-controls="rolePermissions">
                     <i class="link-icon" data-feather="anchor"></i>
                     <span class="link-title">Role & Permission</span>
                     <i class="link-arrow" data-feather="chevron-down"></i>
                 </a>
-                <div class="collapse" id="advancedUI">
+                <div class="collapse" id="rolePermissions">
                     <ul class="nav sub-menu">
                         <li class="nav-item">
                             <a href="{{ route('all.permission') }}" class="nav-link">All Permission</a>
@@ -104,6 +104,25 @@
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('all.role.permission') }}" class="nav-link">All Role in Permission</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item nav-category">Manage Admin User</li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#admin" role="button" aria-expanded="false"
+                    aria-controls="admin">
+                    <i class="link-icon" data-feather="anchor"></i>
+                    <span class="link-title">Manage Admin User</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="admin">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="{{ route('all.admin') }}" class="nav-link">All Admin</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('all.role') }}" class="nav-link">Add Admin</a>
                         </li>
                     </ul>
                 </div>
---
+++ resources/views/backend/pages/admin/add_admin.blade.php
@@ -0,0 +1,56 @@
+@extends('admin.admin_dashboard')
+@section('admin')
+
+<div class="page-content">
+    <div class="row profile-body">
+        <!-- middle wrapper start -->
+        <div class="col-md-8 col-xl-8 middle-wrapper">
+            <div class="row">
+                <div class="card">
+                    <div class="card-body">
+                        <h6 class="card-title">Add Admin</h6>
+                        <form id="myForm" class="forms-sample" method="POST" action="{{ route('store.admin') }}">
+                            @csrf
+                            <div class="mb-3 form-group">
+                                <label for="exampleInputUsername1" class="form-label">Admin User Name</label>
+                                <input type="text" name="username" class="form-control" autocomplete="off">
+                            </div>
+                            <div class="mb-3 form-group">
+                                <label for="exampleInputUsername1" class="form-label">Admin Name</label>
+                                <input type="text" name="name" class="form-control" autocomplete="off">
+                            </div>
+                            <div class="mb-3 form-group">
+                                <label for="exampleInputUsername1" class="form-label">Admin Email</label>
+                                <input type="email" name="email" class="form-control" autocomplete="off">
+                            </div>
+                            <div class="mb-3 form-group">
+                                <label for="exampleInputUsername1" class="form-label">Admin Phone</label>
+                                <input type="text" name="phone" class="form-control" autocomplete="off">
+                            </div>
+                            <div class="mb-3 form-group">
+                                <label for="exampleInputUsername1" class="form-label">Admin Address</label>
+                                <input type="text" name="address" class="form-control" autocomplete="off">
+                            </div>
+                            <div class="mb-3 form-group">
+                                <label for="exampleInputUsername1" class="form-label">Admin Password</label>
+                                <input type="password" name="password" class="form-control" autocomplete="off">
+                            </div>
+                            <div class="mb-3 form-group">
+                                <label for="exampleInputUsername1" class="form-label">Role Name</label>
+                                <select name="roles" class="form-select" id="">
+                                    <option selected="" disable="">Select Role</option>
+                                    @foreach($roles as $role)
+                                    <option value="{{ $role->name }}">{{ $role->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-primary me-2">Save Changes</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
